04a6ad187475d56e37c12f5c66d5a699b1e5098b
droid1989 <sistemapv@puntoverdeleon.mx>
2025-05-23 21:07:49 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mysql =require('promise-mysql')
 
 /**funcion que creara un pool de conexiones  */
-const pool = async ()=> await mysql.createPool({host:'192.168.2.111',user:'dev_pv',password:'pss_dEv_pv_18',port:3306,database:'punto_verde_v2',connectionLimit : 3});
+const pool = async ()=> await mysql.createPool({host:'192.168.1.111',user:'dev_pv',password:'pss_dEv_pv_18',port:3306,database:'punto_verde_v2',connectionLimit : 3});
 // const pool = async ()=> await mysql.createPool({host:'127.0.0.1',user:'root',password:'rootroot',port:3306,database:'punto_verde_2021',connectionLimit : 3});
 
 /**exporta no  la funcion si el pool regresado por la funcion */
